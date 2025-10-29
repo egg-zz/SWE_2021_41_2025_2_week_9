7abc0ab69b867e285fea2c7f78ff36780a7c90a7
eggz6z6 <kazy0606@gmail.com>
2025-10-29 18:46:06 +0900

    modify even_list function.

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from typing import List
 # Skeleton code for even_list
 def even_list(int_list: List[int]) -> List[int]:
     return [x for x in int_list if x % 2 == 0]
-    # TODO: Implement even_list
-    pass
 
 # Skeleton code for sum_of_squares_of_even
 def sum_of_squares_of_even(even_int_list: List[int]) -> int:
